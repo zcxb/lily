abab1ca2d95fe796f477b102fbf8937c0392b6d9
chenjunyao <chen13157379235@outlook.com>
2021-06-18 17:10:15 +0800

    add models

---
+++ templates/lilysimple/src/LilySimple.Infra/EntityTypeConfigurations/RoleEntityTypeConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using LilySimple.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LilySimple.EntityTypeConfigurations
+{
+    public class RoleEntityTypeConfiguration :
+        EntityTypeConfigurationBase<Role>,
+        IEntityTypeConfiguration<Role>
+    {
+        public new void Configure(EntityTypeBuilder<Role> builder)
+        {
+            base.Configure(builder);
+
+            builder.ToTable("role");
+
+            builder.Property(m => m.Name)
+                .HasColumnName("name")
+                .IsRequired()
+                .HasMaxLength(32);
+        }
+    }
+}
---
+++ templates/lilysimple/src/LilySimple.Infra/Models/Role.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LilySimple.Models
+{
+    public class Role : ModelBase
+    {
+        public string Name { get; set; }
+    }
+}

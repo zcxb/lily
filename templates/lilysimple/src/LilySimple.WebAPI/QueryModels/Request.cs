6bb672da95ea493330a77fe139816946a762ef78
chenjunyao <chen13157379235@outlook.com>
2021-07-08 11:18:37 +0800

    update base request namespace

--- templates/lilysimple/src/LilySimple.WebAPI/QueryModels/Request.cs
+++ templates/lilysimple/src/LilySimple.WebAPI/QueryModels/Request.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace LilySimple.QueryModels.Rbac
+namespace LilySimple.QueryModels
 {
     public class Pager
     {

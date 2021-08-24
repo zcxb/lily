9720376561201b99e4bd42174c6c9b1dc159be04
chenjunyao <chen13157379235@outlook.com>
2021-08-24 14:24:05 +0800

    fix: Update user query param's binder name

--- templates/lilysimple/src/LilySimple.WebAPI/QueryModels/Rbac/UserQueryRequest.cs
+++ templates/lilysimple/src/LilySimple.WebAPI/QueryModels/Rbac/UserQueryRequest.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.Json.Serialization;
@@ -8,7 +9,7 @@ namespace LilySimple.QueryModels.Rbac
 {
     public class UserQueryRequest : Pager
     {
-        [JsonPropertyName("username")]
+        [ModelBinder(Name = "username")]
         public string UserName { get; set; }
     }
 }

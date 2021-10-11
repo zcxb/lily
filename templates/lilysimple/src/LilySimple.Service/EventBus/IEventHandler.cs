36fc45f1bd0d4dcff7d6913b5e0a0306d6154cd1
chenjunyao <chen13157379235@outlook.com>
2021-10-11 16:17:28 +0800

    add eventbus handler interface

---
+++ templates/lilysimple/src/LilySimple.Service/EventBus/IEventHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LilySimple.EventBus
+{
+    public interface IEventHandler
+    {
+        public interface IEventHandler
+        {
+        }
+
+        public class EventHandlerBase : IEventHandler
+        {
+            public Contexts.DefaultDbContext Db { protected get; set; }
+
+            public IMediator Bus { protected get; set; }
+
+            public ILogger<EventHandlerBase> Logger { protected get; set; }
+        }
+    }
+}
